847b25b65fe3da6219a9916b7b6d7b20adba78b4
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-12 19:53:33 -0400

    Create authRoutes.js

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,21 @@
+// routes/authRoutes.js
+import express from 'express';
+import passport from 'passport';
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getCurrentUser,
+} from '../controllers/authController.js';
+
+const router = express.Router();
+
+router.post('/register', registerUser);
+
+router.post('/login', passport.authenticate('local'), loginUser);
+
+router.post('/logout', logoutUser);
+
+router.get('/me', getCurrentUser);
+
+export default router;
