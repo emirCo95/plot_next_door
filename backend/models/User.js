d3f2ec071d5f43fb64ce5d78e74450bcc010191c
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-12 20:13:02 -0400

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,11 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  role: {
+    type: String,
+    required: true,
+    enum: ['farmer', 'customer'],
+  },
 });
 
 export default mongoose.model('User', UserSchema);
