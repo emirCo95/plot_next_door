d90a46a83149bebde0235c17a3a87c4ee2272494
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-17 19:37:29 -0400

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -24,28 +24,114 @@ export default function Login() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="p-4 max-w-sm mx-auto">
-      <h2 className="text-xl font-bold mb-2">Login</h2>
-      <input
-        className="border p-2 w-full mb-2"
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        className="border p-2 w-full mb-2"
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button
-        type="submit"
-        className="bg-green-500 text-white w-full py-2 rounded"
-      >
-        Login
-      </button>
-    </form>
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        {/* <img
+          alt="Your Company"
+          src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+          className="mx-auto h-10 w-auto"
+        /> */}
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-charcoal">
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm/6 font-medium text-charcoal"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                autoComplete="email"
+                onChange={(e) => setEmail(e.target.value)}
+                className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-black placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm/6 font-medium text-charcoal"
+              >
+                Password
+              </label>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-indigo-400 hover:text-indigo-300"
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                autoComplete="current-password"
+                onChange={(e) => setPassword(e.target.value)}
+                className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-black placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-cosmic-latte px-3 py-1.5 text-sm/6 font-semibold text-black cursor-pointer"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm/6 text-gray-400">
+          Not a member?{' '}
+          <a
+            href="#"
+            className="font-semibold text-indigo-400 hover:text-indigo-300"
+          >
+            Start a 14 day free trial
+          </a>
+        </p>
+      </div>
+    </div>
   );
 }
+
+// <form onSubmit={handleSubmit} className="p-4 max-w-sm mx-auto">
+//   <h2 className="text-xl font-bold mb-2">Login</h2>
+//   <input
+//     className="border p-2 w-full mb-2"
+//     type="email"
+//     placeholder="Email"
+//     value={email}
+//     onChange={(e) => setEmail(e.target.value)}
+//   />
+//   <input
+//     className="border p-2 w-full mb-2"
+//     type="password"
+//     placeholder="Password"
+//     value={password}
+//     onChange={(e) => setPassword(e.target.value)}
+//   />
+//   <button
+//     type="submit"
+//     className="bg-green-500 text-white w-full py-2 rounded"
+//   >
+//     Login
+//   </button>
+// </form>
