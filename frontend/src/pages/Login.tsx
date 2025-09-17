04f4bbeb6c1d5eaf0370a60f1f786ba02036eb78
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-17 19:39:29 -0400

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -105,27 +105,3 @@ export default function Login() {
     </div>
   );
 }
-
-// <form onSubmit={handleSubmit} className="p-4 max-w-sm mx-auto">
-//   <h2 className="text-xl font-bold mb-2">Login</h2>
-//   <input
-//     className="border p-2 w-full mb-2"
-//     type="email"
-//     placeholder="Email"
-//     value={email}
-//     onChange={(e) => setEmail(e.target.value)}
-//   />
-//   <input
-//     className="border p-2 w-full mb-2"
-//     type="password"
-//     placeholder="Password"
-//     value={password}
-//     onChange={(e) => setPassword(e.target.value)}
-//   />
-//   <button
-//     type="submit"
-//     className="bg-green-500 text-white w-full py-2 rounded"
-//   >
-//     Login
-//   </button>
-// </form>
