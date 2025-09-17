814f61b77b25058ec52139a24dcbdb83a54cd79a
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-17 19:39:51 -0400

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -16,7 +16,7 @@ export default function Login() {
     e.preventDefault();
     try {
       await loginUser({ email, password });
-      toast('Logged in successfully!');
+      toast('Signed in successfully!');
     } catch (error) {
       console.log(error);
       toast('Invalid credentials');
@@ -26,11 +26,6 @@ export default function Login() {
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        {/* <img
-          alt="Your Company"
-          src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-          className="mx-auto h-10 w-auto"
-        /> */}
         <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-charcoal">
           Sign in to your account
         </h2>
