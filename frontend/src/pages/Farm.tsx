5c3197024e9fb4d64e0fbccca07449701f263b5e
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-18 19:24:07 -0400

    Update Farm.tsx

--- frontend/src/pages/Farm.tsx
+++ frontend/src/pages/Farm.tsx
@@ -1,7 +1,16 @@
+import Spinner from '@/components/Spinner';
 import { useFarm } from '@/hooks/useFarm';
 const Farm = () => {
   const { farm, loading } = useFarm();
-  console.log(farm, loading);
+
+  if (loading) {
+    return (
+      <div className="h-[100vh] flex items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
   return (
     <div className="h-[100vh] flex items-center justify-center">
       {farm?.name}
