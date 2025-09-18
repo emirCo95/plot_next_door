113e017e60ee25244c4ca15f5fd712e3269cd333
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-18 11:01:20 -0400

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -33,7 +33,7 @@ export default function Navbar() {
   }
 
   return (
-    <nav>
+    <nav className="fixed w-full top-0 left-0backdrop-blur-md">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-26">
           {/* Logo */}
