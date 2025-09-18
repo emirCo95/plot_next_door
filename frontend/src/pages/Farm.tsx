615eb7504bb89107afb1dc0c347a05f4182eb23c
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-18 19:22:43 -0400

    Update Farm.tsx

--- frontend/src/pages/Farm.tsx
+++ frontend/src/pages/Farm.tsx
@@ -2,7 +2,11 @@ import { useFarm } from '@/hooks/useFarm';
 const Farm = () => {
   const { farm, loading } = useFarm();
   console.log(farm, loading);
-  return <div>Farm</div>;
+  return (
+    <div className="h-[100vh] flex items-center justify-center">
+      {farm?.name}
+    </div>
+  );
 };
 
 export default Farm;
