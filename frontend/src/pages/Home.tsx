8ed90c155d09c78a32d3fb9f553639de5375e8f8
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-18 17:28:47 -0400

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,14 @@
+import { ArrowRight } from 'lucide-react';
+
 const Home = () => {
   return (
     <div className="h-[100vh] bg-gradient-to-b from-white to-cosmic-latte flex items-center justify-center">
-      Home
+      <div className="w-[500px] mx-auto text-center p-2 bg-white/70 rounded-2xl shadow-lg">
+        <p className="flex text-center text-charcoal font-chewy text-lg md:text-xl lg:text-2xl justify-center items-center gap-2">
+          Announcing our latest features. <a href="">Read More</a>
+          <ArrowRight />
+        </p>
+      </div>
     </div>
   );
 };
