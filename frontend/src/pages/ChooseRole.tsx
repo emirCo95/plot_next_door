5c03cd769c76068b69f179703d4ab7317ed2c110
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-18 15:43:27 -0400

    create ChooseRole page

---
+++ frontend/src/pages/ChooseRole.tsx
@@ -0,0 +1,10 @@
+const ChooseRole = () => {
+  return (
+    <div className="h-[100vh] bg-gradient-to-b from-white to-cosmic-latte grid grid-cols-1 md:grid-cols-2">
+      <div className=""></div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default ChooseRole;
