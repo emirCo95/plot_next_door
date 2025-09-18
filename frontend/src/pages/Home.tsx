1e8b3cf05b4b0b89dbd7d8f7385ade7cbf98f5cf
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-18 10:59:37 -0400

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div className="flex items-center justify-center">Home</div>;
+  return <div className="h-screen flex items-center justify-center">Home</div>;
 };
 
 export default Home;
