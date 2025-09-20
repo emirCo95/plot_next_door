40c65ba066a494a1560b13a25cbd252408906956
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-20 16:06:33 -0400

    Update Farm.tsx

--- frontend/src/pages/Farm.tsx
+++ frontend/src/pages/Farm.tsx
@@ -13,7 +13,9 @@ const Farm = () => {
 
   return (
     <div className="h-[100vh] grid grid-cols-1 md:grid-cols-2 bg-gradient-to-b from-white to-cosmic-latte">
-      <div className="flex flex-col items-center justify-center p-8"></div>
+      <div className="flex flex-col items-center justify-center p-8">
+        {farm?.name}
+      </div>
       <div className="flex flex-col items-center justify-center p-8"></div>
     </div>
   );
