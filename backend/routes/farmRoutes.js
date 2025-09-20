665d7526327d1d8a8d61fc013256e858c619b7f3
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-20 16:10:11 -0400

    Update farmRoutes.js

--- backend/routes/farmRoutes.js
+++ backend/routes/farmRoutes.js
@@ -2,10 +2,11 @@ import express from 'express';
 
 import { requireAuth } from '../middleware/authMiddleware.js';
 
-import { fetchFarmDetails } from '../controllers/farmController.js';
+import { fetchFarmDetails, updateFarm } from '../controllers/farmController.js';
 
 const router = express.Router();
 
 router.get('/', requireAuth, fetchFarmDetails);
+router.put('/', requireAuth, updateFarm);
 
 export default router;
